f0eb0af0334443615e4cd02e5e60505f079ad744
ATHIRA <ATHIRA@LAPTOP-BNJ2H6K5>
2024-08-01 21:34:31 +0530

    for testing

---
+++ src/com/obs/obsquaretraining/others/A.java
@@ -0,0 +1,9 @@
+package com.obs.obsquaretraining.others;
+
+public class A {
+	public int marks=10;
+	public void show() {
+		System.out.println("showing");
+	}
+
+}
---
+++ src/com/obs/obsquaretraining/sampleprograms/AreaOfTriangle.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.obs.obsquaretraining.sampleprograms;
+
+/**import com.obs.obsquaretraining.sampleprograms.Area;
+ * 
+ */
+public class AreaOfTriangle {
+	com.obs.obsquaretraining.sampleprograms.Area aa= new com.obs.obsquaretraining.sampleprograms
+	aa.calculate(20,30);
+}
