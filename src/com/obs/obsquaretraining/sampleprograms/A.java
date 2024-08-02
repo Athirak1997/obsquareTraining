8180755d83b666a7ec97779031933446445f7952
ATHIRA <ATHIRA@LAPTOP-BNJ2H6K5>
2024-08-02 10:48:01 +0530

    second commit

---
+++ src/com/obs/obsquaretraining/sampleprograms/A.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.obs.obsquaretraining.sampleprograms;
+import com.obs.obsquaretraining.sampleprograms.B;
+
+
+/**
+ * 
+ */
+public class A {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+B obj= new B();
+obj.display();
+	}
+
+}
---
+++ src/com/obs/obsquaretraining/sampleprograms/Demo.java
@@ -0,0 +1,14 @@
+package com.obs.obsquaretraining.sampleprograms;
+import com.obs.obsquaretraining.others.A;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		A obj =new A();
+		System.out.println(obj.marks);
+		obj.show();
+		// TODO Auto-generated method stub
+
+	}
+
+}
