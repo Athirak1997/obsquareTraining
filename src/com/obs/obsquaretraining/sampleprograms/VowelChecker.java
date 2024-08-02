61b48920a52553b6dd6e26d200c0cc0e04216c59
ATHIRA <ATHIRA@LAPTOP-BNJ2H6K5>
2024-08-02 20:54:49 +0530

    vowel checker commit

---
+++ src/com/obs/obsquaretraining/sampleprograms/VowelChecker.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.obs.obsquaretraining.sampleprograms;
+
+/**
+ * @author ATHIRA
+ */
+public class VowelChecker {
+
+
+	public static void main(String[] args) {
+		char check='a';
+		switch(check) {
+		case 'a':System.out.println("the given letter is a vowel");
+		break;
+		case 'e':System.out.println("the given letter is a vowel");
+		break;
+		case 'i':System.out.println("the given letter is a vowel");
+		break;
+		case'o':System.out.println("the given letter is a vowel");
+		break;
+		case 'u':System.out.println("the given letter is a vowel");
+		break;
+		default:System.out.println("the given letter is a consonent");
+		}
+
+	}
+
+}
