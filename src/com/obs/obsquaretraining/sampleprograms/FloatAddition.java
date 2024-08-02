613ca38678031c949abf540e7811aace32bb7293
ATHIRA <ATHIRA@LAPTOP-BNJ2H6K5>
2024-08-02 20:43:03 +0530

    float addition commit

---
+++ src/com/obs/obsquaretraining/sampleprograms/FloatAddition.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.obs.obsquaretraining.sampleprograms;
+
+/**
+ * 
+ */
+public class FloatAddition {
+	public static void main(String[] args) {
+		float  a= 10f;
+		float b=20f;
+		float sum=a+b;
+		System.out.println("sum = " +sum);
+
+	}
+
+}
