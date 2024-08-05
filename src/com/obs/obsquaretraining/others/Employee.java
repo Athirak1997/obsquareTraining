3c6575a1eb8503fa28fbc3a3294540c40184812b
ATHIRA <ATHIRA@LAPTOP-BNJ2H6K5>
2024-08-05 14:17:48 +0530

    employee commit

--- src/com/obs/obsquaretraining/others/Employee.java
+++ src/com/obs/obsquaretraining/others/Employee.java
@@ -20,7 +20,7 @@ static String company=" ABC";
 	e.display();
 	Employee e1= new Employee(2,"anjana");
 			e1.display();
-			Employee e2= new Employee(3,"arathi");
+			Employee e2= new Employee(3,"aarathi");
 			e2.display();
 
 	}
