f7d7c251078e554156eb0fbce92d0cb23d2820c7
Athirak1997 <athiraskrishnan97@gmail.com>
2024-08-05 14:12:53 +0530

    Add files via upload
    vowelchecker

---
+++ VowelChecker.java
@@ -0,0 +1,19 @@
+class VowelChecker{
+public static void main(String[] args){
+char check='b';
+switch (check)
+{
+	case 'a':System.out.println("The given letter is a vowel");
+	break;
+	case 'e':System.out.println("The given letter is a vowel");
+	break;
+	case 'i':System.out.println("The given letter is a vowel");
+	break;
+	case'o':System.out.println("The given letter is a vowel");
+	break;
+	case 'u':System.out.println("The given letter is a vowel");
+	break;
+	default:System.out.println("The given letter is a consonent");
+}
+}
+}
